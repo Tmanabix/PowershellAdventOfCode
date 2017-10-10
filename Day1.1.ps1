0ffa6b6d46aeea31f752b4b2464691558181e2bd
Tmanabix <32681450+Tmanabix@users.noreply.github.com>
2017-10-10 17:44:20 +0100

    Lesson 1 part 1 complete

---
+++ Day1.1.ps1
@@ -0,0 +1,23 @@
+﻿$floor = 0
+$Plan = Get-Content -Path '.\Prod\AdventOfCode\SteveTi\2015Day1.txt'
+
+
+$plansplit = $Plan -split '' #checked using measure, 7002 count
+
+foreach ($action in $plansplit)
+{
+    #-match '(' $UpFloor
+    if ($action -eq "(") {$floor++} 
+    if  
+    ($action -eq ")") {$floor--}
+    if 
+        ($floor -eq "-1") {"in the basement"}
+}
+
+$floor
+
+
+
+
+
+    
